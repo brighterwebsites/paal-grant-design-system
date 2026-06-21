c67439e113159e94a53fa39b3815e165333a387b
Vanessa Wood <brighterwebsitedesign@gmail.com>
2026-06-21 14:08:01 +1000

    Delete v32/Paal Grant Design System v32 directory

--- v32/Paal Grant Design System v32/components/badges/Badge.d.ts
+++
@@ -1,5 +0,0 @@
-export interface BadgeProps {
-  /** Visual style */
-  variant?: 'section-label' | 'award' | 'size' | 'tag' | 'breadcrumb' | 'gold-title' | 'size-badge';
-  children?: React.ReactNode;
-}
